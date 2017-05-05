46a735f7bec06f35c984cf4ad16a97a0db62fead
龚欢 <gonghuan2020@gmail.com>
2017-05-05 14:18:47 +0800

    GateWayObject init

---
+++ Example/Pods/Target Support Files/GateWayObject/GateWayObject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GateWayObject : NSObject
+@end
+@implementation PodsDummy_GateWayObject
+@end
---
+++ Example/Pods/Target Support Files/Pods-GateWayObject_Tests/Pods-GateWayObject_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GateWayObject_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GateWayObject_Tests
+@end
